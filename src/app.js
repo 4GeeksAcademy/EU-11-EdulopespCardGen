6b8acbfe8f92871e69470ec1f8e7dd8ba86caf8b
Edulopesp <129979638+Edulopesp@users.noreply.github.com>
2023-05-31 19:27:00 +0000

    added timer and modified size

--- src/app.js
+++ src/app.js
@@ -7,12 +7,29 @@ import "./assets/img/4geeks.ico";
 
 const theSymbolElement = document.getElementsByClassName("theSymbol");
 const numberElement = document.getElementById("theNumber");
+const widthElement = document.getElementById("inputWidth");
+const heightElement = document.getElementById("inputHeight");
+const cardElement = document.getElementById("cardSize");
 
 function getRandomElementFromArray(array) {
   return array[Math.floor(Math.random() * array.length)];
 }
 const symbol = ["♦", "♥", "♠", "♣"];
 const numbers = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+
+widthElement.addEventListener("input", function() {
+  cardElement.style.width = widthElement.value + "px";
+  if (widthElement.value < 285) {
+    cardElement.style.width = "285px";
+  }
+});
+
+heightElement.addEventListener("input", function() {
+  cardElement.style.height = heightElement.value + "px";
+  if (heightElement.value < 444) {
+    cardElement.style.height = "444px";
+  }
+});
 
 window.onload = function() {
   //write your code here
@@ -26,4 +43,9 @@ window.onload = function() {
     }
   }
   numberElement.innerHTML = randomNumber;
+
+  const timerInterval = setInterval(timerReload, 10000);
+  function timerReload() {
+    location.reload();
+  }
 };
